6275810b69b4663c1c34d249292f1b28a29e9f4e
Julius Sipos <julius.sipos98@gmail.com>
2021-04-12 20:17:06 +0200

    Deleted Old Update Framework

--- jsdizajner-framework.php
+++ jsdizajner-framework.php
@@ -16,33 +16,33 @@ define('PLUGIN_DIR',plugin_dir_path(__FILE__));
 
 include(plugin_dir_path(__FILE__) . 'includes/class.config.php');
 
-// function create_setting_page()
-// {
-//     add_menu_page(
-//         __('Framework Settings', 'jsdizajner_framework'), // Page Title
-//         __('Framework', 'jsdizajner_framework'), // Menu Item
-//         'manage_options', // User Capabilities
-//         'theme_framework', // Plugin Slug
-//         'render_framework_settings', // Render Page Function
-//         'dashicons-media-code', // Menu Icon
-//         100 // Menu position
-//     );
+function create_setting_page()
+{
+    add_menu_page(
+        __('Framework Settings', 'jsdizajner_framework'), // Page Title
+        __('Framework', 'jsdizajner_framework'), // Menu Item
+        'manage_options', // User Capabilities
+        'theme_framework', // Plugin Slug
+        'render_framework_settings', // Render Page Function
+        'dashicons-media-code', // Menu Icon
+        100 // Menu position
+    );
 
-// }
-// add_action('admin_menu', 'create_setting_page');
+}
+add_action('admin_menu', 'create_setting_page');
 
 
-// function render_framework_settings()
-// {
-//     // Double check user capabilities
-//     if (!current_user_can('manage_options')) {
-//         return;
-//     }
-//     // Get front-end template of the Page / View Template
-//     include(PLUGIN_DIR . 'views/settings.php');
-// }
+function render_framework_settings()
+{
+    // Double check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+    // Get front-end template of the Page / View Template
+    include(PLUGIN_DIR . 'views/settings.php');
+}
 
-// Add a link to your settings page in your plugin
+//Add a link to your settings page in your plugin
 function add_settings_plugin_page($links)
 {
     $settings_link = '<a href="admin.php?page=theme_framework">' . 'Settings' . '</a>';
--- views/settings.php
+++ views/settings.php
@@ -5,15 +5,14 @@
 
 <?php
 
-$MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://jsdizajner.local/update/?action=get_metadata&slug=jsdizajner-framework', //Metadata URL.
-    __FILE__, //Full path to the main plugin file.
-    'jsdizajner-framework' //Plugin slug. Usually it's the same as the name of the directory.
-);
 
 
 ?>
 
-<form method="get" action="<?php $MyUpdateChecker; ?>">
+<form method="post" action="<?php Puc_v4_Factory::buildUpdateChecker(
+                                'https://jsdizajner.local/update/?action=get_metadata&slug=jsdizajner-framework', //Metadata URL.
+                                __FILE__, //Full path to the main plugin file.
+                                'jsdizajner-framework' //Plugin slug. Usually it's the same as the name of the directory.
+                            ); ?>">
 <input type="submit" value="Update">
 </form>
