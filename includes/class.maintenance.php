52581956db9bbc69159a99e15e87f18d14e0113a
Julius Sipos <julius.sipos98@gmail.com>
2021-04-14 16:09:12 +0200

    Documented Functionality

--- includes/class.maintenance.php
+++ includes/class.maintenance.php
@@ -1,24 +1,39 @@
 <?php
 defined('ABSPATH') || exit;
 
-
 class Maintenance 
 {
+
+    /**
+     * Field of all options from Database
+     *
+     * @var [array]
+     */
 
     private $options;
 
     public function __construct()
     {
+        // Set $options as array of data from settings
         $this->options = get_option('framework_settings_option_name');
         if ( class_exists('View') & array_key_exists('maintenance_mode_0', $this->options ) )
             $this->check_status();
     }
+
+    /**
+     * Check if the checkbox is set
+     */
 
     public function check_status()
     {
         if (array_key_exists('maintenance_mode_0', $this->options) === true)
             add_action('get_header', array($this, 'maintenance'));
     }
+
+    /**
+     * Set Wordpress to Maintenance mode
+     * Show the HTML from view/maintenance.php
+     */
 
     public function maintenance()
     {
