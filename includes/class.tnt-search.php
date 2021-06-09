4a0e31d0c03af47f1b67e03a3639857c69129e89
Julius Sipos <julius.sipos98@gmail.com>
2021-06-09 18:21:28 +0200

    TNT Class

---
+++ includes/class.tnt-search.php
@@ -0,0 +1,18 @@
+<?php
+defined('ABSPATH') || exit;
+
+use TeamTNT\TNTSearch\TNTSearch;
+
+/**
+ * @docs https://github.com/teamtnt/tntsearch
+ */
+
+class JSD_TNT
+{
+
+    
+
+}
+
+new JSD_TNT;
+?>
